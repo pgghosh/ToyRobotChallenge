430e38afaaf0ade70049057581f8424641047983
Partha Ghosh <parthagghosh@yahoo.co.uk>
2019-09-08 21:57:47 +0100

    Added comments to Direction class.

--- src/main/java/interview/test/zone/toyrobot/model/Direction.java
+++ src/main/java/interview/test/zone/toyrobot/model/Direction.java
@@ -1,5 +1,8 @@
 package interview.test.zone.toyrobot.model;
 
+/**
+ * The order of the Direction enum constants is important as it helps in a neat solution for left and right commands implementation.
+ */
 public enum Direction {
     SOUTH, WEST, NORTH, EAST
 }
